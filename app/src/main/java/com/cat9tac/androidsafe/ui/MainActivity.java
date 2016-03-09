b1de2ee1e7b841d3efeb5398b00e897707857497
cat9tac <v_ypfu@tencent.com>
2016-03-09 16:28:40 +0800

    first commit

---
+++ app/src/main/java/com/cat9tac/androidsafe/ui/MainActivity.java
@@ -0,0 +1,56 @@
+package com.cat9tac.androidsafe.ui;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.cat9tac.androidsafe.R;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private LinearLayout ll_autoguard;
+    private LinearLayout ll_remote;
+    private LinearLayout ll_encrypt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private  void init(){
+        ll_autoguard= (LinearLayout) findViewById(R.id.ll_autoguard);
+        ll_remote= (LinearLayout) findViewById(R.id.ll_remote);
+        ll_encrypt= (LinearLayout) findViewById(R.id.ll_encrypt);
+        ll_autoguard.setOnClickListener(this);
+        ll_remote.setOnClickListener(this);
+        ll_encrypt.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i=new Intent();
+        Bundle bundle=new Bundle();
+        i.setClass(MainActivity.this,SecondActivity.class);
+        switch (v.getId()){
+            case R.id.ll_autoguard:
+                bundle.putInt("FRAGMENT_ID",0);
+                break;
+            case R.id.ll_remote:
+                bundle.putInt("FRAGMENT_ID",1);
+                break;
+            case R.id.ll_encrypt:
+                bundle.putInt("FRAGMENT_ID",2);
+                break;
+        }
+        i.putExtras(bundle);
+        startActivity(i);
+
+    }
+}
---
+++ app/src/main/java/com/cat9tac/androidsafe/ui/SecondActivity.java
@@ -0,0 +1,51 @@
+package com.cat9tac.androidsafe.ui;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cat9tac.androidsafe.R;
+import com.cat9tac.androidsafe.fragment.AutoGuardFragment;
+import com.cat9tac.androidsafe.fragment.EncryptDataFragment;
+import com.cat9tac.androidsafe.fragment.RemoteControlFragment;
+
+public class SecondActivity extends AppCompatActivity {
+    private int fragmentId;
+    private Bundle bundle;
+    private Fragment fragment;
+    private FragmentManager fm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        bundle = getIntent().getExtras();
+        fragmentId = bundle.getInt("FRAGMENT_ID");
+        fm = getFragmentManager();
+        fragment = fm.findFragmentById(R.id.fragment_container);
+        chooseFragment();
+
+
+    }
+
+    private void chooseFragment() {
+        if (fragment == null) {
+            switch (fragmentId) {
+                case 0:
+                    fragment = new AutoGuardFragment();
+                    getFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+                    break;
+                case 1:
+                    fragment = new RemoteControlFragment();
+                    getFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+                    break;
+                case 2:
+                    fragment = new EncryptDataFragment();
+                    getFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+                    break;
+            }
+
+        }
+    }
+}
