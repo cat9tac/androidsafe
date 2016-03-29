0546cf1c079ac8721dc3b09b6af753f12b83430b
cat9tac <v_ypfu@tencent.com>
2016-03-29 11:37:01 +0800

    add wipedata

--- app/src/main/java/com/cat9tac/androidsafe/receiver/MyDeviceAdminReceiver.java
+++ app/src/main/java/com/cat9tac/androidsafe/receiver/MyDeviceAdminReceiver.java
@@ -7,6 +7,7 @@ import android.content.SharedPreferences;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.cat9tac.androidsafe.util.ActivateDeviceAdmin;
 import com.cat9tac.androidsafe.util.SharePreferenceEditor;
 
 import static android.widget.Toast.LENGTH_SHORT;
@@ -17,9 +18,11 @@ import static android.widget.Toast.LENGTH_SHORT;
 public class MyDeviceAdminReceiver extends DeviceAdminReceiver {
     private static int failed_count=0;
     private static  final String FAILED_COUNT="FAILED_COUNT";
+    private static final String IS_DELETE_DATA = "IS_DELETE_DATA";
     //private SharePreferenceEditor sharePreferenceEditor;
     private static SharedPreferences sharedPreferences;
     private static SharedPreferences.Editor editor;
+
 
 
     @Override
@@ -31,6 +34,10 @@ public class MyDeviceAdminReceiver extends DeviceAdminReceiver {
         failed_count++;
         if(failed_count>=c){
             Log.i("DDD","onPasswordFailed"+failed_count+"time");
+        }
+        if(failed_count>=10&&sharedPreferences.getBoolean(IS_DELETE_DATA,false)){
+            ActivateDeviceAdmin.getInstance(context).getmDPM().wipeData(0);
+            Log.i("DDD","wipeData"+failed_count+"time");
         }
 
 
